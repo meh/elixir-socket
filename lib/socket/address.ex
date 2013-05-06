4fb70a7dd43bd4766721b7077b7f425ef30d48c8
meh <meh@schizofreni.co>
2013-05-06 16:37:09 +0200

    address: support strings

--- lib/socket/address.ex
+++ lib/socket/address.ex
@@ -1,4 +1,8 @@
 defmodule Socket.Address do
+  def parse(text) when is_binary text do
+    parse(binary_to_list(text))
+  end
+
   def parse(text) do
     case :inet.parse_address(text) do
       { :ok, ip } ->

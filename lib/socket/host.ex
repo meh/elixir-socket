a3583aefda1d3b453728d484e527d716907de252
meh <meh@schizofreni.co>
2013-05-11 17:33:23 +0200

    host: add a bunch of documentation

--- lib/socket/host.ex
+++ lib/socket/host.ex
@@ -7,6 +7,10 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
+  @doc """
+  Get the hostent by address.
+  """
+  @spec by_address(Socket.Address.t) :: { :ok, t } | { :error, :inet.posix }
   def by_address(address) do
     case :inet.gethostbyaddr(Socket.Address.parse(address)) do
       { :ok, host } ->
@@ -17,6 +21,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostent by address, raising if an error occurs.
+  """
+  @spec by_address!(Socket.Address.t) :: t | no_return
   def by_address!(address) do
     case :inet.gethostbyaddr(Socket.Address.parse(address)) do
       { :ok, host } ->
@@ -27,6 +35,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostent by name.
+  """
+  @spec by_name(binary | char_list) :: { :ok, t } | { :error, :inet.posix }
   def by_name(name) do
     if is_binary(name) do
       name = binary_to_list(name)
@@ -41,6 +53,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostent by name and family.
+  """
+  @spec by_name(binary | char_list, :inet.address_family) :: { :ok, t } | { :error, :inet.posix }
   def by_name(name, family) do
     if is_binary(name) do
       name = binary_to_list(name)
@@ -55,6 +71,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostent by name, raising if an error occurs.
+  """
+  @spec by_name!(binary | char_list) :: t | no_return
   def by_name!(name) do
     if is_binary(name) do
       name = binary_to_list(name)
@@ -69,6 +89,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostent by name and family, raising if an error occurs.
+  """
+  @spec by_name!(binary | char_list, :inet.address_family) :: t | no_return
   def by_name!(name, family) do
     if is_binary(name) do
       name = binary_to_list(name)
@@ -83,6 +107,10 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the hostname of the machine.
+  """
+  @spec name :: String.t
   def name do
     case :inet.gethostname do
       { :ok, name } ->
@@ -90,10 +118,18 @@ defrecord Socket.Host, [:name, :aliases, :type, :length, :list] do
     end
   end
 
+  @doc """
+  Get the interfaces of the machine.
+  """
+  @spec interfaces :: { :ok, [tuple] } | { :error, :inet.posix }
   def interfaces do
     :inet.getifaddrs
   end
 
+  @doc """
+  Get the interfaces of the machine, raising if an error occurs.
+  """
+  @spec interfaces! :: [tuple] | no_return
   def interfaces! do
     case :inet.getifaddrs do
       { :ok, ifs } ->

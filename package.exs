0e3b9d07adbbfaf8b34b1fa4e816e40dce75ace9
meh <meh@schizofreni.co>
2013-05-25 10:23:30 +0200

    package: add package definition

---
+++ package.exs
@@ -0,0 +1,9 @@
+Expm.Package.new(
+  name: "socket",
+  description: "Elixir socket wrapping library.",
+  version: "0.0.1",
+  licenses: [[name: "WTFPL"]],
+  keywords: ["socket", "wrapper", "websocket", "tcp", "udp", "ssl"],
+  maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
+  repositories: [[github: "meh/elixir-socket", tag: "v0.0.1"]]
+)

3b29e95e63a2f81d80814975fd3f816813444a18
meh <meh@schizofreni.co>
2013-07-16 22:14:23 +0200

    mix: allow for non-NIF

--- mix.exs
+++ mix.exs
@@ -4,19 +4,27 @@ defmodule Socket.Mixfile do
   def project do
     [ app: :socket,
       version: "0.1.2",
-      elixir: "~> 0.9.4",
+      elixir: "~> 0.9.4 or ~> 0.10.0",
       deps: deps ]
   end
 
   # Configuration for the OTP application
   def application do
-    [ applications: [:finalizer, :crypto, :ssl],
-      mod: { Socket.Manager, [] } ]
+    if System.get_env("ELIXIR_NO_NIF") do
+      [ applications: [:crypto, :ssl] ]
+    else
+      [ applications: [:finalizer, :crypto, :ssl],
+        mod: { Socket.Manager, [] } ]
+    end
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :finalizer, github: "meh/elixir-finalizer" } ]
+    if System.get_env("ELIXIR_NO_NIF") do
+      []
+    else
+      [ { :finalizer, github: "meh/elixir-finalizer" } ]
+    end
   end
 end

a3e3ea5469ce1af7e44141ae07d5fb8f2dbcc9b0
timCF <llStrainll@gmail.com>
2015-10-07 20:51:40 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Socket.Mixfile do
   def project do
     [ app: :socket,
       version: "0.3.0",
-      elixir: "~> 1.1.1",
+      #elixir: "~> 1.1.1",
       package: package,
       description: "Socket handling library for Elixir" ]
   end

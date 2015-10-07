f03560fce7cd99c28e003636e8c597152477f7ec
tim <llStrainll@gmail.com>
2015-10-07 20:42:11 +0300

    add options params support for WS

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Socket.Mixfile do
   def project do
     [ app: :socket,
       version: "0.3.0",
-      elixir: "~> 1.0.0-rc1",
+      elixir: "~> 1.1.1",
       package: package,
       description: "Socket handling library for Elixir" ]
   end
